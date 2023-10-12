6be2860508fffb3ea38595be2ae1979fd0de9628
Ion Cojocari <69515189+Cojocari-Ion@users.noreply.github.com>
2023-10-12 22:48:57 +0300

    first push

---
+++ controllers/postControllers/posts_controller.go
@@ -0,0 +1,19 @@
+package postControllers
+
+import "net/http"
+
+func Index(response http.ResponseWritter, request *http.Request) {
+
+}
+
+func Add(response http.ResponseWritter, request *http.Request) {
+
+}
+
+func Edit(response http.ResponseWritter, request *http.Request) {
+
+}
+
+func Delete(response http.ResponseWritter, request *http.Request) {
+
+}
