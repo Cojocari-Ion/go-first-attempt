421c2d2c855c1c7471219a722582985826739967
Ion Cojocari <69515189+Cojocari-Ion@users.noreply.github.com>
2023-10-14 22:01:33 +0300

    asd

--- pkg/routes/post_routes.go
+++ pkg/routes/post_routes.go
@@ -1,15 +1,14 @@
 package routes
 
 import (
-    "github.com/gorilla/mux"
-    "github.com/mrDublionka/go-first-attempt/pkg/controllers"
+	"github.com/Cojocari-Ion/go-first-attempt/tree/main/pkg/controllers"
+	"github.com/gorilla/mux"
 )
 
 var RegisterBlogRoutes = func(router *mux.Router) {
-    router.HandleFunc("/posts/", controllers.CreatePost).Methods("POST")
-    router.HandleFunc("/posts/", controllers.GetPosts).Methods("GET")
-    router.HandleFunc("/posts/{postId}", controllers.GetPostById).Methods("GET")
-    router.HandleFunc("/posts/{postId}", controllers.UpdatePost).Methods("PUT")
-    router.HandleFunc("/posts/{postId}", controllers.DeletePost).Methods("DELETE")
+	router.HandleFunc("/posts/", controllers.CreatePost).Methods("POST")
+	router.HandleFunc("/posts/", controllers.GetPosts).Methods("GET")
+	router.HandleFunc("/posts/{postId}", controllers.GetPostById).Methods("GET")
+	router.HandleFunc("/posts/{postId}", controllers.UpdatePost).Methods("PUT")
+	router.HandleFunc("/posts/{postId}", controllers.DeletePost).Methods("DELETE")
 }
-
